fe353ee3227a8d4f127f0eaad47ad54ae1eb1dc4
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-03-01 19:25:47 -0500

    create env

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/noahfranco/Downloads/first-django-app/playing-with-django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
