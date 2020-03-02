063521d32765f9ffe9c7d30219d03eedc34f081a
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-03-01 19:53:05 -0500

    added classes

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,6 +6,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length= 100)
     pub_data = models.DateTimeField("date published")
 
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length= 200)
     votes = models.IntegerField(default= 0)
