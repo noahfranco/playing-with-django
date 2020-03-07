21c301297098e78126a613c352b1dd133b584421
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-03-06 21:49:20 -0500

    workin on models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,11 +5,16 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length= 100)
     pub_data = models.DateTimeField("date published")
-
+    
+    def __init__(self):
+        return self.question_text
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length= 200)
     votes = models.IntegerField(default= 0)
     question = models.ForeignKey(Question, on_delete= models.CASCADE)
+
+    def __init__(self):
+        return self.choice_text
 
     
